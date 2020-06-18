a8e7214de7051630b738a75848f1551f59a7dd3d
Mcriado <mcriado.cg@gmail.com>
2020-06-17 21:02:12 -0400

    Validación dato tipo email

--- js/validar.js
+++ js/validar.js
@@ -1,20 +1,3 @@
-// function validar(){
-//     var nombre
-//     nombre = document.getElementById("nombre").value;
-
-//     if (nombre === ""){
-//         alert("Todos los campos son obligatorios");
-//         return false;
-//     }
-//     else if (nombre.length>30){
-//         alert("El nombre es muy largo");
-//         return false;
-//     }
-
-//     alert(saludo);
-// }
-
-
 function validar() {
     var nombre = document.getElementById('nombre').value;
     var apellidos = document.getElementById('apellidos').value;
@@ -22,31 +5,18 @@ function validar() {
     var usuario = document.getElementById('usuario').value;
     var pass = document.getElementById('pass').value;
     var phone = document.getElementById('telefono').value;
-    // var btn = document.getElementById('btn');
-
-    // document.addEventListener('DOMContentLoaded', () => {
-    //     btn.disabled = true;
-    // });
 
     if(nombre ==='' || apellidos ==='' || correo ==='' || usuario ==='' || pass ==='' || phone ==='') {
         alert('Todos los campos deben ser completados');
-        return false;
     } else {
-        alert(`Hola ${nombre} ${apellidos} tu datos de registro son: 
+        alert(`
+        Bienvenido!!
+        Hola ${nombre} ${apellidos} tu datos de registro son los siguientes:
         Usuario: ${usuario} 
         Contraseña: ${pass}`);
         return true;
     }
-    
-    // datatype()
-    // function datatype() {
-    //     let nombre
-    //     if(nombre.typeof === Text) {
-    //         console.log(nombre)
-    //     }
-    // }
 
-    console.log('enviando');
     return false;
 }
 
@@ -65,24 +35,27 @@ function caracteres() {
         alert('Los apellidos no pueden superar los 80 caracteres');
     }
     if(campoCorreo.length > 100) {
-        alert('Excede el límite');
+        alert('El correo no puede superar los 100 caracteres');
     }
     if(campoUsuario.length > 20) {
-        alert('Excede el límite');
+        alert('El nombre de usuario no puede exceder los 20 caracteres');
     }
     if(campoPass.length > 10) {
-        alert('Excede el límite');
+        alert('La constraseña solo puede contener 10 caracteres');
     }
     if(campoPhone.length > 15) {
-        alert('Excede el límite');
+        alert('Número invalido');
     }
-    // else if (!(typeof campoNombre ==='text')) {
-    //     alert('malo');
-    // }
-    
     else if (isNaN(campoPhone)) {
-        alert('Ingresa un número valido');
+        alert('Ingresa un número valido por favor');
     }
 }
 
+function validateEmail() {
+    var validaCorreo = document.getElementById('correo').value;
+    var validaEmail = /\w+@\w+\.+[a-z]/;
 
+    if (!validaEmail.test(validaCorreo)) {
+        alert('El correo ingresado es incorrecto');
+    }
+}
